d5d76ce9bee72c4ba6ec267bd432e5a2a3b1e8ba
Jong jae Park <pjj186@naver.com>
2021-02-22 19:54:05 +0900

    initial Commit

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
+import { format } from "morgan";
 import {videos} from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
 
@@ -17,7 +19,20 @@ export const search = (req, res) => {
     // { searchingBy } 는 { searchingBy : searchingBy } 와 같음.
 };
 
-export const upload = (req, res) => res.render("upload", {pageTitle : "Upload"});
+export const getUpload = (req, res) => 
+    res.render("upload", {pageTitle : "Upload"});
+
+export const postUpload = (req, res) => {
+    const {
+        body: {
+            file,
+            title,
+            description
+        }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "VideoDetail"});
 
