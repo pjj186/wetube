0c2b55de4815ab03971cc94cbdbffd20478b50ed
Jong jae Park <pjj186@naver.com>
2021-03-02 15:33:18 +0900

    Footer, Login & Join

--- middlewares.js
+++ middlewares.js
@@ -9,7 +9,7 @@ export const localMiddleware = (req, res, next) => {
     res.locals.siteName = 'WeTube';
     res.locals.routes = routes; // routes.js 를 로컬로 선언함으로써 pug에서도 사용 가능
     res.locals.user = {
-        isAuthenticated: true,
+        isAuthenticated: false,
         id : 1
     };
     next();
